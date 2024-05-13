09079045df0fa1fa2d4d90a0b88330f031f2cf50
juansebastianwebdev <lmjuansebas@gmail.com>
2024-05-13 11:51:12 -0500

    done caching

--- app/my-homes/page.tsx
+++ app/my-homes/page.tsx
@@ -3,8 +3,10 @@ import prisma from "../lib/db";
 import { redirect } from "next/navigation";
 import NoItmes from "../components/NoItmes";
 import ListingCard from "../components/ListingCard";
+import { unstable_noStore as noStore } from "next/cache";
 
 async function getData(userId: string) {
+  noStore();
   const data = await prisma.home.findMany({
     where: {
       userId: userId,
